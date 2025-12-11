18352c8403908f3a7d9594c6664046a5ca6e731f
a-barret <aidenb360@gmail.com>
2025-12-10 23:16:21 -0700

    Initial Commit

---
+++ TicTacToe/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Aiden Barrett on 12/10/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
