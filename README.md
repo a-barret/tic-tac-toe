81a334129d51d291593a12e7ada8ebb3f4ad6532
a-barret <aidenb360@gmail.com>
2025-12-12 16:18:31 -0700

    Removed overly simple function and added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+
+Alternating between player X and player O, allow the current player to select a square on a 3x3 board. If after a piece is placed, there are three Xs in a row or three Os in a row, either diagonally, horizontally or vertically, then the player whose icons form three in a row wins.
+
+# Requirements
+
+- Display the board to the current player before they select a square.
+- End game after all squares are full or one player's icons form three in a row vertially, horizontally, or diagonally or if the current players chooses to end the game.
+- Allow player to select a space that is empty to place their icon.
--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -8,16 +8,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int is_player_o_turn(int turn_count);
-
 char get_current_player_icon(int turn_count);
 
 void execute_turn(char board[9], char current_player_icon, int user_selection);
 
 int main(void) {
     // Start the program with a welcome message and directions.
-    printf("Welcome to Tic Tac Toe!");
-    printf("Enter 0 to quit the game or enter an integer corresponding to one of these spaces:");
+    printf("Welcome to Tic Tac Toe!\n");
+    printf("Enter 0 to quit the game or enter an integer corresponding to one of these spaces:\n");
     printf(" 1 | 2 | 3 \n");
     printf("---+---+---\n");
     printf(" 4 | 5 | 6 \n");
@@ -39,11 +37,12 @@ int main(void) {
         
         if (user_selection >= 1 && user_selection <= 9) {
             execute_turn(board, current_player_icon, user_selection);
-            if (turn_count >= 9) { // also add code to check if game is won.
+            if (turn_count >= 8) { // also add code to check if game is won.
                 in_progress = 0;
             }
+            turn_count++;
         } else if (user_selection == 0) {
-            printf("Goodbye!");
+            printf("Goodbye!\n");
             in_progress = 0;
         } else {
             printf("Invalid input. Try again.");
@@ -52,12 +51,8 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-int is_player_o_turn(int turn_count) {
-    return turn_count % 2;
-}
-
 char get_current_player_icon(int turn_count) {
-    if (is_player_o_turn(turn_count)) {
+    if (turn_count % 2) {
         return 'O';
     } else {
         return 'X';
